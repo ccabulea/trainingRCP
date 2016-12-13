dd52b274edf65b29736baa5fafd097aad5cd76d9
ccabulea <ccabulea@VMDEV04.optilogistic.dom>
2016-12-13 17:22:19 +0100

    rental perspective + icons for views and perspective

---
+++ com.optilogistic.rental.ui/src/com/optilogistic/rental/ui/perspective/RentalPerspective.java
@@ -0,0 +1,39 @@
+package com.optilogistic.rental.ui.perspective;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		String editorArea = layout.getEditorArea();
+		addFastViews(layout);
+		addViewShortcuts(layout);
+		addPerspectiveShortcuts(layout);
+		layout.addView("com.optilogistic.rental.ui.rentalAgencyView", IPageLayout.LEFT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("com.optilogistic.rental.ui.propertyView", IPageLayout.RIGHT, 0.5f, IPageLayout.ID_EDITOR_AREA);
+	}
+
+	/**
+	 * Add fast views to the perspective.
+	 */
+	private void addFastViews(IPageLayout layout) {
+	}
+
+	/**
+	 * Add view shortcuts to the perspective.
+	 */
+	private void addViewShortcuts(IPageLayout layout) {
+	}
+
+	/**
+	 * Add perspective shortcuts to the perspective.
+	 */
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+	}
+
+}
