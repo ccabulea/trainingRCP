e3468c4a6b813b69a21a9230d283d0e4bd46eb5b
ccabulea <ccabulea@VMDEV04.optilogistic.dom>
2016-12-14 17:01:26 +0100

    RCP application

--- com.optilogistic.rental.ui/src/com/optilogistic/rental/ui/perspective/RentalPerspective.java
+++ com.optilogistic.rental.ui/src/com/optilogistic/rental/ui/perspective/RentalPerspective.java
@@ -5,6 +5,7 @@ import org.eclipse.ui.IPerspectiveFactory;
 
 public class RentalPerspective implements IPerspectiveFactory {
 
+	public static final String ID="com.optilogistic.rental.ui.perspective";
 	/**
 	 * Creates the initial layout for a page.
 	 */
